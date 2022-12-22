86cff20987f0426951e9aaa107e1463670a827ff
bioaddict <rafgangbadja@gmail.com>
2022-12-22 04:03:51 +0100

    Fix bug

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,4 +1,4 @@
- Installs a Nginx server with custome HTTP header
+# Installs a Nginx server with custome HTTP header
 
 exec {'update':
   provider => shell,
@@ -15,7 +15,7 @@ exec {'install Nginx':
 exec { 'add_header':
   provider    => shell,
   environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/sites-available/default',
   before      => Exec['restart Nginx'],
 }
 
