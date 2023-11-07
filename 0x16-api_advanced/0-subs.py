e31d20734291b247e505df46b9c4c3df3103b66b
bioaddict <rafgangbadja@gmail.com>
2023-11-07 23:20:00 +0100

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns
+the number of subscribers (not active
+users, total subscribers) for a given
+subreddit. If an invalid subreddit is
+given, the function should return 0.
+"""
+
+import requests as rq
+
+
+def number_of_subscribers(subreddit):
+    """Get the total number of subscribers
+    for a given subreddit. If not a valid
+    subreddit, return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent":"linux:0x16.api.advanced"
+            }
+    response = rq.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
