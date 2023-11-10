0d9035548f796cd682a5639346e6d1929a1a5259
bioaddict <rafgangbadja@gmail.com>
2023-11-10 23:19:16 +0100

    Improve the code

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -53,4 +53,4 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
         [print("{}: {}".format(k, v)) for k, v in instances]
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        return count_words(subreddit, word_list, instances, after, count)
