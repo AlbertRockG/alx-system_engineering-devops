87c5507d3325d822b5c8cae5665b527167571690
bioaddict <rafgangbadja@gmail.com>
2023-11-17 02:07:20 +0100

    Fix styling

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,10 +3,10 @@
 exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+  before   => Exec['restart']
 }
 
-exec {
+exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
